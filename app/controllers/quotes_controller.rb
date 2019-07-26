e8a46a431b7f5f4263797ca033ee7053c7b1268a
Sloane Siverson <sloanems@aol.com>
2019-07-26 03:06:55 +0000

    Add a good message describing what you actually changed since your last commit in here

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,14 +1,21 @@
 class QuotesController < ApplicationController
+  
   def index
+    
     @quote = Quote.order("RANDOM()").first
+  
   end
 
   def new
     @quote = Quote.new
   end
 
-  def create
-    Quote.create(quote_params)
+ def create
+ @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
+  
     redirect_to root_path
   end
 
